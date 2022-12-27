7f1a6a3b69871f0fa52e221681f5e1f0bec9c6f7
ibrahim nady <89800344+ibrahimnady@users.noreply.github.com>
2022-12-27 15:22:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Route path='Salse' element={<Salse />}/>
         <Route path='Products' element={<Products />}/>
         <Route path='ProductSalse' element={<ProductSalse />}/>
-        <Route path='*' element={<h1>404 Not Found</h1>}/>
+        <Route path='*' element={<Home/>}/>
       </Routes>
 
 
